98ad7996f360ab77bd17ac20ef540b68c26d34f7
U-Phyrexia\mmiyaji <mmiyaji@Phyrexia.(none)>
2013-07-10 00:10:42 +0900

    fixed #6 image margin

--- Kiritori/SnapWindow.Designer.cs
+++ Kiritori/SnapWindow.Designer.cs
@@ -62,6 +62,7 @@
             // 
             this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(276, 230);
             this.pictureBox1.TabIndex = 0;
